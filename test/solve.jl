e753e0413522117fd1905f6c1c8a32414d173050
milktrader <milktrader@gmail.com>
2013-11-04 19:54:01 -0500

    tests updated and passing

--- test/solve.jl
+++ test/solve.jl
@@ -1,17 +1,13 @@
-#df = read_monthlies(Pkg.dir("PortfolioModel/test/data/monthlies.csv"))
 #df = readtime(Pkg.dir("PortfolioModels/test/data/monthlies.csv"))
 df = readtime(Pkg.dir("PortfolioModels/test/data/monthlies_parsable.csv"))
 
 g  = globalmin(df)
 
-@assert 0.4562045516784078    == g[1,1] #  0.4562 from Zivot's derivation
-#@assert 0.2232518040053832   == g[1,2] #  0.2233  
-@assert 0.22325180400538316   == g[1,2] #  0.2233  
-#@assert 0.051227252608354694 == g[1,3] #  0.0512   
-@assert 0.051227252608354576  == g[1,3] #  0.0512   
-#@assert 0.26931639170785443  == g[1,4] #  0.2693 
-@assert 0.2693163917078543    == g[1,4] #  0.2693 
-
+@assert  0.456204551678408  == g[1,1] #  0.4562 from Zivot's derivation
+@assert 0.2232518040053831  == g[1,2] #  0.2233  
+#@assert 0.223251804005383  == g[1,2] #  0.2233  
+@assert 0.05122725260835459 == g[1,3] #  0.0512   
+@assert 0.2693163917078543   == g[1,4] #  0.2693 
 
 # > muhat.vals
 #    Boeing Nordstrom Starbucks Microsoft 
