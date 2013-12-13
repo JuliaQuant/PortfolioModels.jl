67813adceebff8368c5cd19d4748d63527ca4226
milktrader <milktrader@gmail.com>
2013-12-13 13:46:42 -0500

    changed to test approx_eq

--- test/solve.jl
+++ test/solve.jl
@@ -3,11 +3,11 @@ df = readtime(Pkg.dir("PortfolioModels/test/data/monthlies_parsable.csv"))
 
 g  = globalmin(df)
 
-@assert  0.456204551678408  == g[1,1] #  0.4562 from Zivot's derivation
-@assert 0.2232518040053831  == g[1,2] #  0.2233  
-#@assert 0.223251804005383  == g[1,2] #  0.2233  
-@assert 0.05122725260835459 == g[1,3] #  0.0512   
-@assert 0.2693163917078543   == g[1,4] #  0.2693 
+@test_approx_eq 0.456204551678408   g[1,1] #  0.4562 from Zivot's derivation
+@test_approx_eq 0.2232518040053831  g[1,2] #  0.2233  
+@test_approx_eq 0.223251804005383   g[1,2] #  0.2233  
+@test_approx_eq 0.05122725260835459 g[1,3] #  0.0512   
+@test_approx_eq 0.2693163917078543  g[1,4] #  0.2693 
 
 # > muhat.vals
 #    Boeing Nordstrom Starbucks Microsoft 
